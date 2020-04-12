99dbcf0c65f3ca1fb955e4e836cedc1b5908ae49
SmylerMC <smyler@mail.com>
2020-04-12 15:53:55 +0200

    Setup main mod files from example mod

--- src/main/java/fr/smyler/mcct/MCCT.java
+++ src/main/java/fr/smyler/mcct/MCCT.java
@@ -1,8 +1,8 @@
-package net.fabricmc.example;
+package fr.smyler.mcct;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class MCCT implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.

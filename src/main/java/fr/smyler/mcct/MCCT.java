22ca8374037f653f3a820eb4bffd574658853c25
SmylerMC <smyler@mail.com>
2020-04-12 16:04:28 +0200

    Initialize as a client mod only

--- src/main/java/fr/smyler/mcct/MCCT.java
+++ src/main/java/fr/smyler/mcct/MCCT.java
@@ -1,10 +1,10 @@
 package fr.smyler.mcct;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 
-public class MCCT implements ModInitializer {
+public class MCCT implements ClientModInitializer {
 	@Override
-	public void onInitialize() {
+	public void onInitializeClient() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.

02f98c47a6e432af1107899baf65be206757dc19
SmylerMC <smyler@mail.com>
2020-04-22 11:26:31 +0200

    Fully implemented switch hands key in survival inventory

--- src/main/java/fr/smyler/mcct/mixin/SwitchHandsTweakMixin.java
+++ src/main/java/fr/smyler/mcct/mixin/SwitchHandsTweakMixin.java
@@ -9,12 +9,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.ContainerProvider;
 import net.minecraft.client.gui.screen.ingame.ContainerScreen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.container.Container;
 import net.minecraft.container.Slot;
 import net.minecraft.container.SlotActionType;
 import net.minecraft.text.Text;
 
-//TODO Only works with the survival inventory, implement it for creative and make sure it is not triggered in chests or other containers
+//TODO Only works with the survival inventory, implement it for creative
 @Mixin(ContainerScreen.class)
 public abstract class SwitchHandsTweakMixin<T extends Container> extends Screen implements ContainerProvider<T> {
 
@@ -26,9 +27,8 @@ public abstract class SwitchHandsTweakMixin<T extends Container> extends Screen 
 
 	@Inject(at=@At("HEAD"), method="handleHotbarKeyPressed")
 	private void handleHotbarKeyPressed(int keyCode, int scanCode, CallbackInfoReturnable<Boolean> info) {
-		if (this.minecraft.player.inventory.getCursorStack().isEmpty() && this.focusedSlot != null) {
-			if (minecraft.options.keySwapHands.matchesKey(keyCode, scanCode)) {
-				System.out.println("Swaping! Focused:" + focusedSlot.id);
+		if (this.minecraft.player.inventory.getCursorStack().isEmpty() && this.focusedSlot != null && this.focusedSlot.id >= 9) {
+			if (minecraft.options.keySwapHands.matchesKey(keyCode, scanCode) && this.minecraft.currentScreen instanceof InventoryScreen) {
 				minecraft.interactionManager.clickSlot(this.getContainer().syncId, 45, 0, SlotActionType.PICKUP, this.minecraft.player);
 				minecraft.interactionManager.clickSlot(this.getContainer().syncId, this.focusedSlot.id, 0, SlotActionType.PICKUP, this.minecraft.player);
 				minecraft.interactionManager.clickSlot(this.getContainer().syncId, 45, 0, SlotActionType.PICKUP, this.minecraft.player);

da05e4ec7356442aa4e80da9e92e03f29c50134f
SmylerMC <smyler@mail.com>
2020-04-25 15:54:12 +0200

    Basic mixin to tweak the lava fog density

---
+++ src/main/java/fr/smyler/mcct/mixin/BackgroundRendererMixin.java
@@ -0,0 +1,25 @@
+package fr.smyler.mcct.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+
+import net.minecraft.client.render.BackgroundRenderer;
+import net.minecraft.client.render.Camera;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.tag.FluidTags;
+
+@Mixin(BackgroundRenderer.class)
+public abstract class BackgroundRendererMixin {
+
+	@Inject(at=@At("TAIL"), method="applyFog")
+	private static void fogTweak(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, CallbackInfo info) {
+		FluidState fluidState = camera.getSubmergedFluidState();
+		if (fluidState.matches(FluidTags.LAVA)) {
+			RenderSystem.fogDensity(0.25F);
+         }
+	}
+}

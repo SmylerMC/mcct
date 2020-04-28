3f697eb559339445021ebc2d96d484a527473447
SmylerMC <smyler@mail.com>
2020-04-28 16:44:30 +0200

    Fixed lava tweak with Optifine using a new Optifine specific mixin set

--- src/main/java/fr/smyler/mcct/mixin/BackgroundRendererMixin.java
+++ src/main/java/fr/smyler/mcct/mixin/BackgroundRendererMixin.java
@@ -12,18 +12,29 @@ import net.minecraft.client.render.Camera;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.tag.FluidTags;
 
-//FIXME Not compatible with Optifine
+/**
+ * 
+ * This mixin only targets the vanilla applyFog method
+ * Optifine patches the rendering code in such a way that applyFog is not used anymore,
+ * and this mixin is therefore useless when Optifabric and Optifine are present.
+ * In such a case, we instead need to inject into Optifine's setupFog method, which is absent in vanilla.
+ * This is done in BackgroundRendererOptifineMixin
+ * 
+ * @author SmylerMC
+ * 
+ */
 @Mixin(BackgroundRenderer.class)
 public abstract class BackgroundRendererMixin {
-	
-	public static float lavaFogDensity = 0.25F;
+
+	private static float lavaFogDensity = 0.25F; //TODO Have this somewhere else
 
 	@Inject(at=@At("TAIL"), method="applyFog")
 	private static void fogTweak(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, CallbackInfo info) {
 		FluidState fluidState = camera.getSubmergedFluidState();
 		if (fluidState.matches(FluidTags.LAVA)) {
 			RenderSystem.fogDensity(lavaFogDensity);
-         }
+		}
 	}
-	
+
+
 }

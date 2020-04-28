7ade5c332b0a60384a043b9a8078bfc0f784f339
SmylerMC <smyler@mail.com>
2020-04-28 18:11:27 +0200

    Merged the two lava tweak mixins in one

--- src/main/java/fr/smyler/mcct/mixin/BackgroundRendererMixin.java
+++ src/main/java/fr/smyler/mcct/mixin/BackgroundRendererMixin.java
@@ -9,16 +9,10 @@ import com.mojang.blaze3d.systems.RenderSystem;
 
 import net.minecraft.client.render.BackgroundRenderer;
 import net.minecraft.client.render.Camera;
-import net.minecraft.fluid.FluidState;
 import net.minecraft.tag.FluidTags;
 
 /**
- * 
- * This mixin only targets the vanilla applyFog method
- * Optifine patches the rendering code in such a way that applyFog is not used anymore,
- * and this mixin is therefore useless when Optifabric and Optifine are present.
- * In such a case, we instead need to inject into Optifine's setupFog method, which is absent in vanilla.
- * This is done in BackgroundRendererOptifineMixin
+ * Changes the lava fog density
  * 
  * @author SmylerMC
  * 
@@ -26,15 +20,45 @@ import net.minecraft.tag.FluidTags;
 @Mixin(BackgroundRenderer.class)
 public abstract class BackgroundRendererMixin {
 
+	// Vanilla default is 2.0F
 	private static float lavaFogDensity = 0.25F; //TODO Have this somewhere else
 
+	/**
+	 * Inject the tweak into applyFog, which is the vanilla fog method
+	 * This injection should always be successful, but applyFog is not called anymore if Optifine is installed
+	 * 
+	 * @param camera
+	 * @param fogType
+	 * @param viewDistance
+	 * @param thickFog
+	 * @param info
+	 */
 	@Inject(at=@At("TAIL"), method="applyFog")
 	private static void fogTweak(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, CallbackInfo info) {
-		FluidState fluidState = camera.getSubmergedFluidState();
-		if (fluidState.matches(FluidTags.LAVA)) {
-			RenderSystem.fogDensity(lavaFogDensity);
-		}
+		mcctLavaFogTweak(camera, fogType, viewDistance, thickFog);
+	}
+	
+	/**
+	 * Injects the tweak into setupFog, which is Optifine's fog method
+	 * We need this separate injection as the vanilla applyFog method is not used when Optifine is installed
+	 * setupFog does not exist in the vanilla game, this injection will therefore fail if Optifine is not installed
+	 * 
+	 * @param camera
+	 * @param fogType
+	 * @param viewDistance
+	 * @param thickFog
+	 * @param afloat
+	 * @param info
+	 */
+	@Inject(at=@At("TAIL"), method="setupFog", require=0) //TODO Make sure the injection is successful if Optifine is installed
+	private static void fogTweakOptifine(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, float afloat, CallbackInfo info) {
+		mcctLavaFogTweak(camera, fogType, viewDistance, thickFog);
+	}
+	
+	private static void mcctLavaFogTweak(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog) {
+		if (camera.getSubmergedFluidState().matches(FluidTags.LAVA)) RenderSystem.fogDensity(lavaFogDensity);
 	}
 
+	
 
 }

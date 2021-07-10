597bf90f5bd05ca831af82bfa5c6d4e263bcd392
SmylerMC <smyler@mail.com>
2021-07-10 16:59:11 +0200

    Fixed toast rendering

--- src/main/java/fr/thesmyler/mcct/gui/toasts/GenericToast.java
+++ src/main/java/fr/thesmyler/mcct/gui/toasts/GenericToast.java
@@ -3,6 +3,7 @@ package fr.thesmyler.mcct.gui.toasts;
 import com.mojang.blaze3d.systems.RenderSystem;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.resource.language.I18n;
 import net.minecraft.client.toast.Toast;
 import net.minecraft.client.toast.ToastManager;
@@ -10,50 +11,50 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 
 public class GenericToast implements Toast {
-	
-	private boolean justUpdated = true;
-	private long startTime;
-	protected String titleKey;
-	protected String descriptionKey;
-	protected ItemStack icon;
-	
-	public GenericToast(String titleKey, String descriptionKey, ItemStack icon) {
-		this.titleKey = titleKey;
-		this.descriptionKey = descriptionKey;
-		this.icon = icon;
-	}
-		
-	@Override
-	public Visibility draw(MatrixStack matrices, ToastManager manager, long currentTime) {
-		if (this.justUpdated) {
-			this.startTime = currentTime;
-			this.justUpdated = false;
-		}
 
-		MinecraftClient client = manager.getGame();
-		client.getTextureManager().bindTexture(TEXTURE);
-		RenderSystem.clearColor(1.0F, 1.0F, 1.0F, 1.0F);
-//	    RenderSystem.color3f(1.0F, 1.0F, 1.0F); //TODO Is this right ?
-		manager.drawTexture(matrices, 0, 0, 0, 32, 160, 32);
-		float xpos = this.icon != null ? 30: 10;
-		if (this.descriptionKey == null) {
-			client.textRenderer.draw(matrices, this.getLocalizedTitle(), xpos, 12, 0x000000);
-		} else {
-			client.textRenderer.draw(matrices, this.getLocalizedTitle(), xpos, 7, 0x000000);
-			client.textRenderer.draw(matrices, this.getLocalizedDescription(), xpos, 18, 0xFF2222);
-		}
-		
-		if(this.icon != null) manager.getGame().getItemRenderer().renderGuiItemIcon(this.icon, 8, 8);
-		
-		return currentTime - this.startTime < 5000L ? Toast.Visibility.SHOW : Toast.Visibility.HIDE;
-	}
-	
-	public String getLocalizedDescription() {
-		return I18n.translate(this.descriptionKey);
-	}
-	
-	public String getLocalizedTitle() {
-		return I18n.translate(this.titleKey);
-	}
+    private boolean justUpdated = true;
+    private long startTime;
+    protected String titleKey;
+    protected String descriptionKey;
+    protected ItemStack icon;
+
+    public GenericToast(String titleKey, String descriptionKey, ItemStack icon) {
+        this.titleKey = titleKey;
+        this.descriptionKey = descriptionKey;
+        this.icon = icon;
+    }
+
+    @Override
+    public Visibility draw(MatrixStack matrices, ToastManager manager, long currentTime) {
+        if (this.justUpdated) {
+            this.startTime = currentTime;
+            this.justUpdated = false;
+        }
+
+        MinecraftClient client = manager.getGame();
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderTexture(0, TEXTURE);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        manager.drawTexture(matrices, 0, 0, 0, 32, 160, 32);
+        float xpos = this.icon != null ? 30: 10;
+        if (this.descriptionKey == null) {
+            client.textRenderer.draw(matrices, this.getLocalizedTitle(), xpos, 12, 0x000000);
+        } else {
+            client.textRenderer.draw(matrices, this.getLocalizedTitle(), xpos, 7, 0x000000);
+            client.textRenderer.draw(matrices, this.getLocalizedDescription(), xpos, 18, 0xFF2222);
+        }
+
+        if(this.icon != null) manager.getGame().getItemRenderer().renderGuiItemIcon(this.icon, 8, 8);
+
+        return currentTime - this.startTime < 5000L ? Toast.Visibility.SHOW : Toast.Visibility.HIDE;
+    }
+
+    public String getLocalizedDescription() {
+        return I18n.translate(this.descriptionKey);
+    }
+
+    public String getLocalizedTitle() {
+        return I18n.translate(this.titleKey);
+    }
 
 }
